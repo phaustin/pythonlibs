0c54e42752071e582b88e0bdb7340afcab87bb8f
Phil Austin <paustin@eos.ubc.ca>
2015-05-02 14:00:37 -0700

    python3 version

--- pyutils/crossref.py
+++ pyutils/crossref.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python2
-#encoding: utf-8
+#!/usr/bin/env python3
+
+## In [7]: print sys.stdout.encoding
+## ------> print(sys.stdout.encoding)
+## UTF-8
+
 
 #http://kitchingroup.cheme.cmu.edu/blog/2014/11/04/Accessing-web-of-science-entry-citing-and-related-articles-from-a-doi-in-emacs/
 #originally from http://www.thamnos.de/misc/look-up-bibliographical-information-from-a-doi/
@@ -10,12 +14,17 @@
 # crossref.py 10.1021/jp047349j
 # http://ws.isiknowledge.com/cps/openurl/service?url_ver=Z39.88-2004&rft_id=info:doi/10.1021/jp047349j
 
+from __future__ import print_function
 import os,site
 home_dir=os.getenv('HOME')
 site.addsitedir('%s/repos' % home_dir)
-
+import pdb
 
 from pythonlibs.pyutils.bitly_helper import get_connection
+
+def ascii_filt(my_string):
+    return ''.join([i for i in my_string if ord(i) < 128])
+
 
 bitly_conn=get_connection()
 
@@ -25,13 +34,13 @@ crossref_api_key = 'paustin@eos.ubc.ca'
  
 # get the doi
 import sys
-from string import strip
+
 for arg in sys.argv[1:]:
-    arg = strip(arg)
-    arg = strip(arg, chars="doi:")
-    arg = strip(arg, chars="http://")
-    arg = strip(arg, chars="dx.doi.org/")
-    doi = strip(arg)
+    arg = arg.strip()
+    arg = arg.strip("doi:")
+    arg = arg.strip("http://")
+    arg = arg.strip("dx.doi.org/")
+    doi = arg.strip()
 
     wos_url=r"http://ws.isiknowledge.com/cps/openurl/service?url_ver=Z39.88-2004&rft_id=info:doi/{}".format(doi)
     citing_url=r"http://ws.isiknowledge.com/cps/openurl/service?url_ver=Z39.88-2004&rft_id=info%3Adoi%2F{}&svc_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Asch_svc&svc.citing=yes".format(doi)
@@ -55,13 +64,13 @@ for arg in sys.argv[1:]:
     # download the xml
     import urllib
     from xml.dom import minidom
-    usock = urllib.urlopen('http://www.crossref.org/openurl/?id=doi:'+doi+'&noredirect=true&pid='+crossref_api_key+'&format=unixref')
+    usock = urllib.request.urlopen('http://www.crossref.org/openurl/?id=doi:'+doi+'&noredirect=true&pid='+crossref_api_key+'&format=unixref')
     xmldoc = minidom.parse(usock)
     usock.close()
  
     if debug:
         out=xmldoc.toxml()
-        print(out.encode('ascii','ignore'))
+        print(out)
     print("\n")
  
     a = xmldoc.getElementsByTagName("doi_records")[0]
@@ -72,32 +81,33 @@ for arg in sys.argv[1:]:
     journal_meta = d.getElementsByTagName("journal_metadata")[0]
     journal_title = journal_meta.getElementsByTagName("full_title")[0]
     abbrev_title = journal_meta.getElementsByTagName("abbrev_title")[0]
-    text_journal_title = journal_title.firstChild.data.encode('ascii', 'ignore')
-    abbrev_journal_title = abbrev_title.firstChild.data.encode('ascii', 'ignore')
+    #pdb.set_trace()
+    text_journal_title = journal_title.firstChild.data
+    abbrev_journal_title = abbrev_title.firstChild.data
     journal_issue = d.getElementsByTagName("journal_issue")[0]
     date = journal_issue.getElementsByTagName("publication_date")[0]
     year = date.getElementsByTagName("year")[0]
-    text_year = year.firstChild.data.encode('ascii', 'ignore')
+    text_year = year.firstChild.data
     resource = d.getElementsByTagName("resource")[0]
-    resource=resource.firstChild.data.encode('ascii', 'ignore')
+    resource=resource.firstChild.data
  
     try:
         journal_volume = journal_issue.getElementsByTagName("journal_volume")[0]
         volume = journal_issue.getElementsByTagName("volume")[0]
-        text_volume = volume.firstChild.data.encode('ascii', 'ignore')
+        text_volume = volume.firstChild.data
     except IndexError:
         pass
  
     try:
         issue = journal_issue.getElementsByTagName("issue")[0]
-        text_issue = issue.firstChild.data.encode('ascii', 'ignore')
+        text_issue = issue.firstChild.data
     except IndexError:
         pass
  
     journal_article = d.getElementsByTagName("journal_article")[0]
     titles = journal_article.getElementsByTagName("titles")[0]
     title = titles.getElementsByTagName("title")[0]
-    text_title = title.firstChild.data.encode('ascii', 'ignore')
+    text_title = title.firstChild.data
  
     contributors = journal_article.getElementsByTagName("contributors")[0]
     for person_name in contributors.getElementsByTagName("person_name"):
@@ -105,9 +115,9 @@ for arg in sys.argv[1:]:
         text_surname = ""
         # get names
         given_name = person_name.getElementsByTagName("given_name")[0]
-        text_given_name = given_name.firstChild.data.encode('ascii', 'ignore')
+        text_given_name = given_name.firstChild.data
         surname = person_name.getElementsByTagName("surname")[0]
-        text_surname = surname.firstChild.data.encode('ascii', 'ignore')
+        text_surname = surname.firstChild.data
         authorlist.append(text_surname+", "+text_given_name)
         #first author?
         sequence = person_name.attributes.getNamedItem("sequence")
@@ -120,12 +130,12 @@ for arg in sys.argv[1:]:
         pages = None
     try:
         first_page = pages.getElementsByTagName("first_page")[0]
-        text_first_page = first_page.firstChild.data.encode('ascii', 'ignore')
+        text_first_page = first_page.firstChild.data
     except:
         pass
     try:
         last_page = pages.getElementsByTagName("last_page")[0]
-        text_last_page = last_page.firstChild.data.encode('ascii', 'ignore')
+        text_last_page = last_page.firstChild.data
     except:
         pass
     # physical review
@@ -136,28 +146,29 @@ for arg in sys.argv[1:]:
             pages = None
         try:
             first_page = pages.getElementsByTagName("item_number")[0]
-            text_first_page = first_page.firstChild.data.encode('ascii', 'ignore')
+            text_first_page = first_page.firstChild.data
         except:
             pass
  
     # output
- 
-    print "@ARTICLE{"+text_first_author_surname+text_year[-2:]+","
-    print "author = {"+" and ".join(authorlist)+"},"
-    print "title = {"+text_title+"},"
-    print "journal = {"+abbrev_journal_title+"},"
+
+    ascii_author=ascii_filt(text_first_author_surname)
+    print("@ARTICLE{"+ascii_author+text_year[-2:]+",")
+    print("author = {"+" and ".join(authorlist)+"},")
+    print("title = {"+text_title+"},")
+    print("journal = {"+abbrev_journal_title+"},")
     if not text_volume == "":
-        print "volume = {"+text_volume+"},"
+        print("volume = {"+text_volume+"},")
     if not text_issue == "":
-        print "number = {"+text_issue+"},"
-    print "year = {"+text_year+"},"
+        print("number = {"+text_issue+"},")
+    print("year = {"+text_year+"},")
     if ((text_first_page != "") and (text_last_page != "")):
-        print "pages = {"+text_first_page+"-"+text_last_page+"},"
+        print("pages = {"+text_first_page+"-"+text_last_page+"},")
     if ((text_first_page != "") and (text_last_page == "")):
-        print "pages = {"+text_first_page+"},"
-    print "doi = {"+doi+"},"
-    print "resource = {http://ezproxy.library.ubc.ca/login?url="+resource+"},"
-    print "wos = {{{}}},".format(wos_url)
-    print "citing = {{{}}},".format(citing_url)
-    print "related = {{{}}},".format(related_url)
-    print "}"
+        print("pages = {"+text_first_page+"},")
+    print("doi = {"+doi+"},")
+    print("resource = {http://ezproxy.library.ubc.ca/login?url="+resource+"},")
+    print("wos = {{{}}},".format(wos_url))
+    print("citing = {{{}}},".format(citing_url))
+    print("related = {{{}}},".format(related_url))
+    print("}")
