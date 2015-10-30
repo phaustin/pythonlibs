9f322c8e2e57dfab97c31b68331fd5eabcdab1e6
Phil Austin <paustin@eos.ubc.ca>
2015-10-30 10:57:17 -0700

    get numbers for vlad

--- diskinventory/tera_recover.py
+++ diskinventory/tera_recover.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 """
+   ipython console:  run with %run -i
+
    reads sqllite databases created by readlist.py  and readdu.py and writes out
     file information
 
@@ -17,111 +19,114 @@ import argparse, textwrap
 import datetime
 import dateutil
 import site,os
+import numpy as np
+
 home_dir=os.getenv('HOME')
 site.addsitedir('{}/repos/'.format(home_dir))
-from pythonlibs.pyutils import check_md5 as c5
+import collections
 
 def get_frame_from_query(the_query,colnames):
     """make a dataframe from an sqlalchemy query"""
     df=DataFrame.from_records(list(the_query),columns=colnames)
     return df
 
-file_db='./files_newtera.db'
-dbstring='sqlite:///{:s}'.format(file_db)
-db = dataset.connect(dbstring)
-session=sessionmaker(bind=db.engine)
-thesession=session()
-file_table=db.metadata.tables['files']
-direc_table=db.metadata.tables['direcs']
+class keep_df:
+    def __init__(self,df_files):
+        self.df_files=df_files
+        self.owners=self.find_owners()
+        hit_owner={}
+        owner_sizes={}
+        for the_owner in the_data.find_owners():
+            hit,the_size=the_data.find_size(the_owner)
+            hit_owner[the_owner]=hit
+            owner_sizes[the_owner]=the_size
+        self.hit_owner=hit_owner
+        self.owner_sizes=owner_sizes
+        self.dirtree=collections.defaultdict(list)
+        self.breakupdir=collections.defaultdict(list)
 
-size=5.e5
-large_query=thesession.query(file_table).filter(file_table.c.size > size).with_labels()
-colnames=[item.name for item in list(db.metadata.tables['files'].columns)]
+    def find_owners(self):
+        owners=set(self.df_files['owner'])
+        return owners
 
-df_files=get_frame_from_query(large_query,colnames)
+    def find_size(self,owner):
+        hit=self.df_files['owner'] == owner
+        sizes=(self.df_files['size'][hit]).sum()
+        return(hit,sizes)
 
-test=df_files.loc[:]
-## records=test.to_dict('records')
-## print(records)
+    def make_tree(self,owner):
+        hit=self.hit_owner[owner]
+        the_dirs=self.df_files[hit]['directory']
+        for a_dir in the_dirs:
+            self.dirtree[owner].append(a_dir.split('/'))
 
-buf_length=int(1.e5)
+    def write_breakup(self,owner,depth):
+        for fragments in self.dirtree[owner]:
+            self.breakupdir[owner].append('/'.join(fragments[:depth]))
+        
+if __name__ == "__main__":
+    if first_trip:
+        file_db='./files_newtera.db'
+        dbstring='sqlite:///{:s}'.format(file_db)
+        db = dataset.connect(dbstring)
+        session=sessionmaker(bind=db.engine)
+        thesession=session()
+        file_table=db.metadata.tables['files']
+        direc_table=db.metadata.tables['direcs']
 
-def fullnames(row):
-    the_file='{}/{}'.format(row['directory'],row['name'])
-    return the_file
+        size=1.e5
+        large_query=thesession.query(file_table).filter(file_table.c.size > size).with_labels()
+        colnames=[item.name for item in list(db.metadata.tables['files'].columns)]
+        df_files_100K=get_frame_from_query(large_query,colnames)
 
-def calc_hash(row):
-    the_file='{}/{}'.format(row['directory'],row['name'])
-    size,start,mid,stop=c5.check_md5(the_file,buf_length)
-    return (size,'{}{}{}'.format(start,mid,stop))
+        size=1.
+        large_query=thesession.query(file_table).filter(file_table.c.size > size).with_labels()
+        colnames=[item.name for item in list(db.metadata.tables['files'].columns)]
+        df_files=get_frame_from_query(large_query,colnames)
+    else:
+        pass
 
-out=test.groupby('size')
-saveit=[]
-for the_size,value in out.groups.items():
-    if len(value) > 1:
-       saveit.append(the_size)
-
-file_count=0
-save_names=[]
-save_fullhash=[]
-
-for framename in saveit:
-    df=out.get_group(framename)
-    save_names.append(df.apply(fullnames,axis=1))
-    save_fullhash.append(df.apply(calc_hash,axis=1))
-    file_count+=len(df)
-    
-    
-
-## for group in the_groups:
-##     df_size=out.get_group(group)
-#print(test.apply(calc_hash,axis=1))
-
-
-## df_files['size']=df_files['size']*1.e-9
-## df_files=df_files.sort(columns='size',ascending=False)
-
-
-
-## large_query=thesession.query(direc_table).filter((direc_table.c.level == 8))
-## df_direcs=get_frame_from_query(large_query)
-## hit=df_direcs['directory'].str.contains('/tera/phil/')
-## out=df_direcs[hit]
-## report=out[['directory','size']]
-## report=report.sort(columns='size',ascending=False)
-## report['size']=report['size']*1.e-6
-## print(report['size'].sum())
-
-## print(report.to_string())
-
-
-## for id,row in report.iterrows():
-##     left_size=len(row['directory'])
-##     padding=70-left_size
-##     row['space']=' '*padding
-##     print("{directory:s}:{space:s}{size:>6.3f}".format(**row))
-
-
-## for id,row in report.iterrows():
-##     left_size=len(row['directory'])
-##     print("rm -rf {directory:s}".format(**row))
-
-big_names=[]
-big_hash=[]
-big_size=[]
-
-for item in save_names:
-    big_names.extend(item.tolist())
-
-for item in save_fullhash:
-    sep_hash=[the_tup[1] for the_tup in item.tolist()]
-    sep_size=[the_tup[0] for the_tup in item.tolist()]
-    big_hash.extend(sep_hash)
-    big_size.extend(sep_size)
-
-saveit=pd.DataFrame()
-saveit['filenames']=big_names
-saveit['fullhash']=big_hash
-saveit['fullsize']=big_size
-with pd.HDFStore('store.h5','w') as store:
-    store.put('big_files',saveit,format='table')
+    # if first_trip:
+    #     with pd.HDFStore('tera_recover.h5','w') as store:
+    #         store.put('all',df_files,format='table')
+    #         store.put('g100K',df_files_100K,format='table')
+    # owners=find_owner(df_files)
+    the_data = keep_df(df_files)
+    the_data.make_tree('vpopa')
+    the_data.write_breakup('vpopa',7)    
+    all_dirs=set(the_data.breakupdir['vpopa'])
+    df_vpopa=df_files[the_data.hit_owner['vpopa']].copy()
+    df_vpopa['breakupdir']=the_data.breakupdir['vpopa']
+    vpopa_groups=df_vpopa.groupby(['breakupdir'])
+    vpopa_size=vpopa_groups['size']
+    vpopa_name=vpopa_groups['name']
+    out=vpopa_size.agg(np.sum)
+    width=80
+    formatstring='{{0:<{}}} {{1:8.3f}}'.format(width)
+    print(formatstring,'\n')
+    sum=0
+    for key,value in out.items():
+        name=key.ljust(width)
+        if value < 1.e9:
+            continue
+        sum+=value
+        print(formatstring.format(key,value*1.e-9))
+        
+    # the_data.make_tree('nchaparr')
+    # the_data.write_breakup('nchaparr',8)    
+    # all_dirs=set(the_data.breakupdir['nchaparr'])
+    # df_nchaparr=df_files[the_data.hit_owner['nchaparr']].copy()
+    # df_nchaparr['breakupdir']=the_data.breakupdir['nchaparr']
+    # nchaparr_groups=df_nchaparr.groupby(['breakupdir'])
+    # nchaparr_size=nchaparr_groups['size']
+    # out=nchaparr_size.agg(np.sum)
+    # width=80
+    # formatstring='{{0:<{}}} {{1:8.3f}}'.format(width)
+    # print(formatstring,'\n')
+    # sum=0
+    # for key,value in out.items():
+    #     name=key.ljust(width)
+    #     if value < 1.e9:
+    #         continue
+    #     sum+=value
+    #     print(formatstring.format(key,value*1.e-9))
