3110040130ea4a74c809a31a2414afebae188157
Phil Austin <paustin@eos.ubc.ca>
2016-03-17 11:41:16 -0700

    fix typos

--- pyutils/killprocs.py
+++ pyutils/killprocs.py
@@ -1,37 +1,37 @@
 #https://pythonhosted.org/psutil/
 import psutil
-
-
-keepit={}
-for proc in psutil.process_iter():
-    try:
-        try:
-            fullname = proc.exe()
-            pinfo = proc
-            keepit[proc.pid] = (proc.create_time(),proc.exe(),proc.cmdline(),proc)
-        except Exception as e:
-            print("Exception: ",e)
-    except psutil.NoSuchProcess:
-        pass
-    else:
-        pass
+from pyutils.helper_funs import make_tuple
+import sys
 
 def on_terminate(proc):
     print("process {} terminated with exit code {}".format(proc, proc.returncode))
 
-proclist=[]    
-for create,name,cmdline,proc in keepit.values():
-    if name.find('mini3') > -1:
-        proclist.append((create,name,cmdline,proc))
 
-proclist.sort()        
-for item in proclist:
-    print(item)
-        
-# for p in proclist:
-#     p.terminate()
-    
-# gone, alive = psutil.wait_procs(proclist, timeout=3, callback=on_terminate)
-    
-# for p in alive:
-#     p.kill()
+if __name__ == "__main__":
+
+    keepit={}
+    keys=['time','name','cmdline','proc']
+    for proc in psutil.process_iter():
+        try:
+            the_dict=dict(zip(keys,(proc.create_time(),proc.exe(),proc.cmdline(),proc)))
+            keepit[proc.pid] = make_tuple(the_dict)
+        except (psutil.ZombieProcess, psutil.AccessDenied,psutil.NoSuchProcess):
+            pass
+
+
+    proclist=[]
+    #
+    # don't kill this process or the emacs python parser
+    #
+    for the_tup in keepit.values():
+        if the_tup.name.find('mini3') > -1 and the_tup.cmdline[-1].find('killprocs') == -1 and \
+            the_tup.cmdline[-1].find('elpy') == -1:
+            proclist.append(the_tup)
+
+    proconly = [item.proc for item in proclist]
+    [proc.terminate() for proc in proconly]
+
+    gone, alive = psutil.wait_procs(proconly, timeout=3, callback=on_terminate)
+
+    for p in alive:
+        p.kill()
