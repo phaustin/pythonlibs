f4bc552e55d920693b6ef2e2ff003215bff41d57
Phil Austin <paustin@eos.ubc.ca>
2016-04-03 10:23:34 -0700

    working on linux

--- pyutils/killprocs.py
+++ pyutils/killprocs.py
@@ -1,36 +1,63 @@
-#https://pythonhosted.org/psutil/
+#!/usr/bin/env python
+"""
+   example:
+
+   python -m pyutils.killprocs mini3
+
+   will kill all process weith mini3 in the name
+   https://pythonhosted.org/psutil/
+"""
 import psutil
 from pyutils.helper_funs import make_tuple
 import sys
+import argparse
+import textwrap
+
 
 def on_terminate(proc):
-    print("process {} terminated with exit code {}".format(proc, proc.returncode))
+    print("process {} terminated with exit code {}".format(
+               proc, proc.returncode))
+
 
 if __name__ == "__main__":
 
-    keepit={}
-    keys=['time','name','cmdline','proc']
+    linebreaks = argparse.RawTextHelpFormatter
+    descrip = textwrap.dedent(globals()['__doc__'])
+    parser = argparse.ArgumentParser(formatter_class=linebreaks,
+                                     description=descrip)
+    parser.add_argument('string', type=str, help='string in processname')
+    args = parser.parse_args()
+
+    keepit = {}
+    keys = ['time', 'name', 'cmdline', 'proc']
     for proc in psutil.process_iter():
         try:
-            the_dict=dict(zip(keys,(proc.create_time(),proc.exe(),proc.cmdline(),proc)))
+            the_dict = dict(zip(keys, (proc.create_time(), proc.exe(),
+                                       proc.cmdline(), proc)))
             keepit[proc.pid] = make_tuple(the_dict)
-        except (psutil.ZombieProcess, psutil.AccessDenied,psutil.NoSuchProcess):
+        except (psutil.ZombieProcess, psutil.AccessDenied,
+                psutil.NoSuchProcess):
             pass
 
-
-    proclist=[]
+    proclist = []
     #
     # don't kill this process or the emacs python parser
     #
     for the_tup in keepit.values():
-        if the_tup.name.find('mini3') > -1 and the_tup.cmdline[-1].find('killprocs') == -1 and \
-            the_tup.cmdline[-1].find('elpy') == -1:
+        string_cmd = ' '.join(the_tup.cmdline)
+        if the_tup.name.find(args.string) > -1 and \
+           string_cmd.find('killprocs') == -1 and \
+           string_cmd.find('elpy') == -1:
             proclist.append(the_tup)
 
     proconly = [item.proc for item in proclist]
+    for item in proconly:
+        cmd_string = ' '.join(item.cmdline())
+        print('terminating: {}'.format(cmd_string))
     [proc.terminate() for proc in proconly]
 
     gone, alive = psutil.wait_procs(proconly, timeout=3, callback=on_terminate)
 
     for p in alive:
         p.kill()
+        
