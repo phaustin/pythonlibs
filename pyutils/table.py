2ee860b851e65c435f72835f69929990a2b2f649
Phil Austin <paustin@eos.ubc.ca>
2016-04-21 09:38:43 -0700

    table and chmod

---
+++ pyutils/table.py
@@ -0,0 +1,44 @@
+#https://pypi.python.org/pypi/tabulate 
+#http://stackoverflow.com/questions/11347505/what-are-some-approaches-to-outputting-a-python-data-structure-to-restructuredte
+
+from __future__ import print_function
+def make_table(grid):
+    max_cols = [max(out)
+                for out in map(list, zip(*[[len(item) for item in row]
+                                           for row in grid]))]
+    rst = table_div(max_cols, 1)
+
+    for i, row in enumerate(grid):
+        header_flag = False
+        if i == 0 or i == len(grid) - 1: header_flag = True
+        rst += normalize_row(row, max_cols)
+        rst += table_div(max_cols, header_flag)
+    return rst
+
+
+def table_div(max_cols, header_flag=1):
+    out = ""
+    if header_flag == 1:
+        style = "="
+    else:
+        style = "-"
+
+    for max_col in max_cols:
+        out += max_col * style + " "
+
+    out += "\n"
+    return out
+
+
+def normalize_row(row, max_cols):
+    r = ""
+    for i, max_col in enumerate(max_cols):
+        r += row[i] + (max_col - len(row[i]) + 1) * " "
+
+    return r + "\n"
+
+
+if __name__ == "__main__":
+    print(make_table([['Name', 'Favorite Food', 'Favorite Subject'], [
+        'Joe', 'Hamburgrs', 'I like things with really long names'
+    ], ['Jill', 'Salads', 'American Idol'], ['Sally', 'Tofu', 'Math']]))
