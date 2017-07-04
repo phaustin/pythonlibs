e879a5e1d4d0f8f48d90bc92930cabcfbd18202d
Phil Austin <paustin@eos.ubc.ca>
2017-07-03 17:04:46 -0700

    checkpoint

--- diskinventory/diskinventory/read_parquet.py
+++ diskinventory/diskinventory/read_parquet.py
@@ -1,37 +1,52 @@
-from dask.dataframe import read_table,read_parquet,to_parquet
+"""
+example:
+
+pythion -m diskinventory.read_parquet pqfiles/newtera out_newtera
+
+"""
+
+from dask.dataframe import read_parquet,to_parquet
 import dask.dataframe
-import pyarrow.parquet as pq
 from pathlib import Path
 import pandas as pd
 import numpy as np
 
-users_dir=Path('/localhome/datatmp/phil/pqfiles/newtera/')
-file_list=users_dir.glob('*pq')
-str_list=[str(item) for item in file_list]
-str_list=sorted(str_list)
+def convert_to_dask(users_dir,out_dir):
+    users_dir=Path(users_dir)
+    file_list=users_dir.glob('*pq')
+    str_list=[str(item) for item in file_list]
+    str_list=sorted(str_list)
 
-da_frame=read_parquet(str_list[0])
-stop=len(da_frame)
-index=np.arange(0,stop)
-da_frame['index']=pd.Series(index)
-da_frame.set_index('index',inplace=True,drop=True)
+    da_frame=read_parquet(str_list[0])
+    stop=len(da_frame)
+    index=np.arange(0,stop)
+    da_frame['index']=pd.Series(index)
+    da_frame.set_index('index',inplace=True,drop=True)
 
-for item in str_list[1:]:
-    print(item)
-    start=stop
-    new_frame=read_parquet(item)
-    stop=start + len(new_frame)
-    index=np.arange(start,stop)
-    new_frame['index']=pd.Series(index)
-    new_frame.set_index('index',inplace=True,drop=True)
-    print(new_frame.divisions)
-    da_frame=dask.dataframe.multi.concat([da_frame,new_frame],interleave_partitions=True)
-    
-print(len(da_frame))
-out_dir='out_newtera'
-to_parquet(out_dir,da_frame)
-# for item in df_list[1:5]:
-#     to_parquet(out_dir,item,append=True)
+    for item in str_list[1:]:
+        print(item)
+        start=stop
+        new_frame=read_parquet(item)
+        stop=start + len(new_frame)
+        index=np.arange(start,stop)
+        new_frame['index']=pd.Series(index)
+        new_frame.set_index('index',inplace=True,drop=True)
+        print(new_frame.divisions)
+        da_frame=dask.dataframe.multi.concat([da_frame,new_frame],interleave_partitions=True)
+        
+    print('writing dataframe: size is ',len(da_frame))
+    to_parquet(out_dir,da_frame)
+
+
+if __name__ == "__main__":    
+    import argparse, textwrap
+    linebreaks = argparse.RawTextHelpFormatter
+    descrip = textwrap.dedent(globals()['__doc__'])
+    parser = argparse.ArgumentParser(formatter_class=linebreaks,
+                                     description=descrip)
+    #parser.add_argument('rootdir',  type=str,help='root directory to be stored as prefix')
+    parser.add_argument('users_dir', type=str, help='directory with pq files created by parse_file.py')
+    parser.add_argument('out_dir', type=str, help='directory for parquet.Dataset')
     
     
 
