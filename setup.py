fc34f6d857b7c1450a27a50c1342ae8bfddfa01a
Phil Austin <paustin@eos.ubc.ca>
2020-04-10 08:25:30 -0700

    checkin

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Setup script for PyPI; use CMakeFile.txt to build extension modules
+
+from setuptools import setup
+
+
+
+setup(
+    name='pythonlib',
+    packages=['pyutils'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License'
+    ],
+    entry_points={
+          'console_scripts': [
+              'get_space = pyutils.get_space:main',
+              'killprocs = pyutils.killprocs:main',
+              'run_shrink = pyutils.run_shrink:main'
+          ]
+    },
+    keywords='C++11, Python bindings',
+    long_description="""pybind11""")
