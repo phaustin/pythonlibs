bdc2d085a04294a4861453ac7c8a590b5e64ffa4
Phil Austin <paustin@eos.ubc.ca>
2020-05-16 13:56:52 -0700

    add psutil as a requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@
 from setuptools import setup
 
 
-
 setup(
     name='pythonlib',
     packages=['pyutils'],
@@ -19,5 +18,6 @@ setup(
               'run_shrink = pyutils.run_shrink:main'
           ]
     },
+    install_requires=['psutil'],
     keywords='C++11, Python bindings',
     long_description="""pybind11""")
